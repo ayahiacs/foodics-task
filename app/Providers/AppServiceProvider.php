af0c96e38eea46ec4ae1561dc6bf2906835f9b14
Ahmed Yahia <ayahiacs@gmail.com>
2023-01-25 08:48:08 +0200

    the task

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,10 @@
 
 namespace App\Providers;
 
+use App\Contracts\Services\IIngredientsStockChecker;
+use App\Contracts\Services\IIngredientsCalculator;
+use App\Services\IngredientsStockChecker;
+use App\Services\IngredientsCalculator;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +17,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->bind(IIngredientsCalculator::class, IngredientsCalculator::class);
+        $this->app->bind(IIngredientsStockChecker::class, function(){
+            return new IngredientsStockChecker($this->app->make(IIngredientsCalculator::class));
+        });
     }
 
     /**
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Ingredient;
+use App\Models\Product;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,11 +17,38 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $beef = Ingredient::create([
+            'id' => 1,
+            'name' => "Beef",
+            'stock_grams' => 20000,
+            'stock_minimum_grams' => 10000,
+            'stock_minimum_notification_enabled' => true,
+        ]);
+        $cheese = Ingredient::create([
+            'id' => 2,
+            'name' => "cheese",
+            'stock_grams' => 5000,
+            'stock_minimum_grams' => 2500,
+            'stock_minimum_notification_enabled' => true,
+        ]);
+        $onion = Ingredient::create([
+            'id' => 3,
+            'name' => "onion",
+            'stock_grams' => 1000,
+            'stock_minimum_grams' => 500,
+            'stock_minimum_notification_enabled' => true,
+        ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $burger = Product::create([
+            'id' => 1,
+            'name' => "Burger",
+        ]);
+
+        $burger->ingredients()->sync([
+            $beef->id => ['grams' => 150],
+            $cheese->id => ['grams' => 30],
+            $onion->id => ['grams' => 20]
+        ]);
+
     }
 }
